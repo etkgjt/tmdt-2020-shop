66ec595715c552e29aaf76d5da0bdb9d65042d6e
etkgjt <canlong102@gmail.com>
2021-06-29 20:05:20 +0700

    update messenger chat

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import React from "react";
+import logo from "./logo.svg";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-import './App.css';
-import { Route } from 'react-router-dom';
-import SingleProduct from './pages/SingleProduct';
-import Home from './pages/Home';
+import "./App.css";
+import { Route } from "react-router-dom";
+import SingleProduct from "./pages/SingleProduct";
+import Home from "./pages/Home";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
-	return (
-		<Provider store={store}>
-			<Route path="/" component={Home} />
-			<Route path="/single_product" component={SingleProduct} />
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <Route path="/" component={Home} />
+      <Route path="/single_product" component={SingleProduct} />
+    </Provider>
+  );
 }
 
 export default App;
